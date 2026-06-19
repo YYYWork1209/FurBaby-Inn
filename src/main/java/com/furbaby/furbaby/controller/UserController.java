ec7596eae51de93ef5c100e7535345956cadaeaf
YYY <3202595713@qq.com>
2026-06-20 00:06:10 +0800

    修复前后端联调代理转发问题

--- src/main/java/com/furbaby/furbaby/controller/UserController.java
+++ src/main/java/com/furbaby/furbaby/controller/UserController.java
@@ -12,12 +12,14 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @Tag(name = "用户管理")
-@RestController("/api/user")
+@RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/user")
 public class UserController {
 
     private final IUserService userService;
